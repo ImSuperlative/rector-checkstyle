b613daff26351e599a8fd060cb74f2fe63518f7f
Ruben Graf <ruben@grebban.com>
2021-04-29 12:36:16 +0200

    init

---
+++ src/CheckstyleOutputFormatter.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Checkstyle;
+
+use Rector\ChangesReporting\Contract\Output\OutputFormatterInterface;
+use Rector\Core\ValueObject\ProcessResult;
+use DOMDocument;
+
+final class CheckstyleOutputFormatter implements OutputFormatterInterface
+{
+    /**
+     * @var string
+     */
+    public const NAME = 'checkstyle';
+
+    /*
+    protected CheckstyleDOMElementFactory $checkstyleDOMElementFactory;
+
+    public function __construct(CheckstyleDOMElementFactory $checkstyleDOMElementFactory)
+    {
+        $this->checkstyleDOMElementFactory = $checkstyleDOMElementFactory;
+    }
+    */
+
+    public function report(ProcessResult $processResult): void
+    {
+        $domDocument = new DOMDocument('1.0', 'UTF-8');
+
+        $checkstyleDOMElementFactory = new CheckstyleDOMElementFactory();
+        $domElement = $checkstyleDOMElementFactory->create($domDocument, $processResult);
+        //$domElement = $this->checkstyleDOMElementFactory->create($domDocument, $processResult);
+        $domDocument->appendChild($domElement);
+
+        // pretty print with spaces
+        $domDocument->formatOutput = true;
+        echo $domDocument->saveXML();
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+}
